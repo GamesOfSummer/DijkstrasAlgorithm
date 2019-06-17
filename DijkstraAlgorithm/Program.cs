15d5138d3ae0a5f58c5b31e730f2e445fd9d4c13
Summer <summer.d.howard@gmail.com>
2019-06-17 10:34:24 -0500

    can find the lowest edge ONCE

--- DijkstraAlgorithm/Program.cs
+++ DijkstraAlgorithm/Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace DijkstraAlgorithm
 {
+
+    class ArrayObject
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int value { get; set; }
+    }
+
+
+
+
     class Program
     {
         static void Main(string[] args)
@@ -15,7 +26,7 @@ namespace DijkstraAlgorithm
                 { 0,5,0,0 },
                 { 0,1,7,0,},
                 { 0,3,9,0 },
-                { 0,0,0,0 }
+                { 0,0,0,1 }
             };
 
 
@@ -31,7 +42,14 @@ namespace DijkstraAlgorithm
 
             }
 
-           
+
+
+
+            var edges = SearchForEdges(array, 0, 0);
+            var lowestValue = edges.OrderBy(x => x.value).First();
+
+
+
 
 
             Console.ReadLine();
@@ -39,26 +57,32 @@ namespace DijkstraAlgorithm
 
 
 
-        private static void SearchForEdges(int[,] array, int x, int y)
+        private static ArrayObject[] SearchForEdges(int[,] array, int x, int y)
         {
+
+            var holder = new List<ArrayObject>();
 
             for (int i = -1; i < 2; i++)
             {
                 for (int j = -1; j < 2; j++)
                 {
 
-                    if((x + i) >= 0 && (x + i) <= (array.GetLength(0) - 1) && 
-                        (y + j) >= 0 && (y + j) <= (array.GetLength(1) - 1))
+                    if(!((x == i) && (y == j)) &&
+                        (x + i) >= 0 && (x + i) <= (array.GetLength(0) - 1) && 
+                        (y + j) >= 0 && (y + j) <= (array.GetLength(1) - 1) &&
+                        array[x + i, y + j] != 0)
                     {
                         Console.Write(array[x + i, y + j]);
+                        holder.Add(new ArrayObject() { x = i, y = j, value = array[x + i, y + j] });
                     }
 
                 }
 
             }
 
-            Console.WriteLine();
 
+            Console.WriteLine();
+            return holder.ToArray();
         }
 
 
